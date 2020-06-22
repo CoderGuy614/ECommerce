c3f45ce732953d3e8e27f9de7781534029b587b2
CoderGuy614 <jlutz.110@gmail.com>
2020-06-22 10:15:01 +0800

    Create SignUp Form Functions, able to register new user

--- Ecommerce-front/src/user/Signup.js
+++ Ecommerce-front/src/user/Signup.js
@@ -1,8 +1,94 @@
-import React from "react";
+import React, { useState } from "react";
+import { API } from "../config";
+
 import Layout from "../core/Layout";
 
-const Signup = () => (
-  <Layout title="Sign Up" description="Sign Up for E-commerce Shop"></Layout>
-);
+const Signup = () => {
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    password: "",
+    error: "",
+    success: false,
+  });
+
+  const { name, email, password } = values;
+
+  const handleChange = (name) => (event) => {
+    setValues({ ...values, error: false, [name]: event.target.value });
+  };
+
+  const signup = (user) => {
+    fetch(`${API}/signup`, {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const clickSubmit = (event) => {
+    event.preventDefault();
+    signup({ name, email, password });
+  };
+
+  const signUpForm = () => (
+    <form>
+      <div className="form-group">
+        <label htmlFor="" className="text-muted">
+          Name
+        </label>
+        <input
+          onChange={handleChange("name")}
+          type="text"
+          className="form-control"
+        />
+      </div>
+
+      <div className="form-group">
+        <label htmlFor="" className="text-muted">
+          Email
+        </label>
+        <input
+          onChange={handleChange("email")}
+          type="email"
+          className="form-control"
+        />
+      </div>
+
+      <div className="form-group">
+        <label htmlFor="" className="text-muted">
+          Password
+        </label>
+        <input
+          onChange={handleChange("password")}
+          type="password"
+          className="form-control"
+        />
+      </div>
+      <button onClick={clickSubmit} className="btn btn-primary">
+        Submit
+      </button>
+    </form>
+  );
+
+  return (
+    <Layout
+      title="Sign Up"
+      description="Sign Up for E-commerce Shop"
+      className="container col-md-8 offset-md-2"
+    >
+      {signUpForm()}
+    </Layout>
+  );
+};
 
 export default Signup;
