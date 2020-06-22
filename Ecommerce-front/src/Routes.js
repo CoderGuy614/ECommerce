7e03cefd770f03f8003222f74c7be51de9ee7376
CoderGuy614 <jlutz.110@gmail.com>
2020-06-22 21:10:34 +0800

    Admin and User Dashboards with Links

--- Ecommerce-front/src/Routes.js
+++ Ecommerce-front/src/Routes.js
@@ -6,7 +6,9 @@ import Signin from "./user/Signin";
 import Home from "./core/Home";
 
 import PrivateRoute from "./auth/PrivateRoute";
+import AdminRoute from "./auth/AdminRoute";
 import Dashboard from "./user/UserDashboard";
+import AdminDashboard from "./user/AdminDashboard";
 
 const Routes = () => {
   return (
@@ -15,7 +17,8 @@ const Routes = () => {
         <Route path="/" exact component={Home} />
         <Route path="/signin" exact component={Signin} />
         <Route path="/signup" exact component={Signup} />
-        <PrivateRoute path="/dashboard" exact component={Dashboard} />
+        <PrivateRoute path="/user/dashboard" exact component={Dashboard} />
+        <AdminRoute path="/admin/dashboard" exact component={AdminDashboard} />
       </Switch>
     </BrowserRouter>
   );
--- Ecommerce-front/src/user/Signin.js
+++ Ecommerce-front/src/user/Signin.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Redirect } from "react-router-dom";
 
 import Layout from "../core/Layout";
-import { signin, authenticate } from "../auth";
+import { signin, authenticate, isAuthenticated } from "../auth";
 
 const Signin = () => {
   const [values, setValues] = useState({
@@ -15,6 +15,7 @@ const Signin = () => {
   });
 
   const { email, password, loading, error, redirectToReferrer } = values;
+  const { user } = isAuthenticated();
 
   const handleChange = (name) => (event) => {
     setValues({ ...values, error: false, [name]: event.target.value });
@@ -77,6 +78,13 @@ const Signin = () => {
 
   const redirectUser = () => {
     if (redirectToReferrer) {
+      if (user && user.role === 1) {
+        return <Redirect to="/admin/dashboard" />;
+      } else {
+        return <Redirect to="user/dashboard" />;
+      }
+    }
+    if (isAuthenticated()) {
       return <Redirect to="/" />;
     }
   };
