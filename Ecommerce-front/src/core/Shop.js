802e54f28772599c0d9f9963a22dc9882e0b8db2
CoderGuy614 <jlutz.110@gmail.com>
2020-06-24 08:18:07 +0800

    Add Filtering Functions

--- Ecommerce-front/src/core/Shop.js
+++ Ecommerce-front/src/core/Shop.js
@@ -5,6 +5,9 @@ import { getCategories } from "../core/apiCore";
 import Checkbox from "./Checkbox";
 
 const Shop = () => {
+  const [myFilters, setMyFilters] = useState({
+    filters: { category: [], price: [] },
+  });
   const [categories, setCategories] = useState([]);
   const [error, setError] = useState("");
 
@@ -23,6 +26,13 @@ const Shop = () => {
     init();
   }, []);
 
+  const handleFilters = (filters, filterBy) => {
+    console.log(filters, filterBy);
+    const newFilters = { ...myFilters };
+    newFilters.filters[filterBy] = filters;
+    setMyFilters(newFilters);
+  };
+
   return (
     <Layout
       title="Shop Page"
@@ -33,10 +43,13 @@ const Shop = () => {
         <div className="col-4">
           <h4>Filter By Category</h4>
           <ul>
-            <Checkbox categories={categories} />
+            <Checkbox
+              categories={categories}
+              handleFilters={(filters) => handleFilters(filters, "category")}
+            />
           </ul>
         </div>
-        <div className="col-8">rightSideBar</div>
+        <div className="col-8">{JSON.stringify(myFilters)}</div>
       </div>
     </Layout>
   );
