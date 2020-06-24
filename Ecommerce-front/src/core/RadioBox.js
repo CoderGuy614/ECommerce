9f3182d6b01b381f61d1f7018da47c2bd7be458d
CoderGuy614 <jlutz.110@gmail.com>
2020-06-24 08:32:14 +0800

    Add Radio Box for Price Filters

---
+++ Ecommerce-front/src/core/RadioBox.js
@@ -0,0 +1,21 @@
+import React, { useState, useEffect, Fragment } from "react";
+
+const RadioBox = ({ prices }) => {
+  const [value, setValue] = useState(0);
+
+  const handleChange = () => {};
+
+  return prices.map((p, i) => (
+    <div key={i} className="list-unstyled">
+      <input
+        onChange={handleChange}
+        type="radio"
+        value={`${p._id}`}
+        className="mr-2 ml-4"
+      />
+      <label className="form-check-label">{p.name}</label>
+    </div>
+  ));
+};
+
+export default RadioBox;
--- Ecommerce-front/src/core/Shop.js
+++ Ecommerce-front/src/core/Shop.js
@@ -3,6 +3,8 @@ import Layout from "./Layout";
 import Card from "./Card";
 import { getCategories } from "../core/apiCore";
 import Checkbox from "./Checkbox";
+import RadioBox from "./RadioBox";
+import { prices } from "./fixedPrices";
 
 const Shop = () => {
   const [myFilters, setMyFilters] = useState({
@@ -48,6 +50,14 @@ const Shop = () => {
               handleFilters={(filters) => handleFilters(filters, "category")}
             />
           </ul>
+
+          <h4>Filter By Price Range</h4>
+          <div>
+            <RadioBox
+              prices={prices}
+              handleFilters={(filters) => handleFilters(filters, "price")}
+            />
+          </div>
         </div>
         <div className="col-8">{JSON.stringify(myFilters)}</div>
       </div>
