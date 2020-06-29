ddd6c6bfc71d367e4c32eaf13c35d22e7c3a56ee
CoderGuy614 <jlutz.110@gmail.com>
2020-06-29 18:42:19 +0800

    Show List of Orders to Admin

--- Ecommerce-front/src/admin/Orders.js
+++ Ecommerce-front/src/admin/Orders.js
@@ -4,6 +4,7 @@ import { isAuthenticated } from "../auth";
 import { Link } from "react-router-dom";
 import { listOrders } from "./apiAdmin";
 import { cloneWith } from "lodash";
+import moment from "moment";
 
 const Orders = () => {
   const [orders, setOrders] = useState([]);
@@ -25,16 +26,75 @@ const Orders = () => {
     loadOrders();
   }, []);
 
-  const noOrders = (orders) => {
-    return orders.length < 1 ? <h4>No Orders Found</h4> : null;
+  const showOrdersLength = () => {
+    if (orders.length > 0) {
+      return (
+        <h1 className="text-danger display-2">Total orders: {orders.length}</h1>
+      );
+    } else {
+      return <h1 className="text-danger">No Orders</h1>;
+    }
+  };
+
+  const showInput = (key, value) => {
+    return (
+      <div className="input-group mb-2 mr-sm-2">
+        <div className="input-group-prepend">
+          <div className="input-group-text">{key}</div>
+        </div>
+        <input type="text" value={value} className="form-control" readOnly />
+      </div>
+    );
   };
 
   return (
     <Layout title="Orders" description={`Hello ${user.name}, View your orders`}>
       <div className="row">
         <div className="col-md-8 offset-md-2">
-          {JSON.stringify(orders)}
-          {noOrders(orders)}
+          {orders.map((o, oIndex) => {
+            return (
+              <div
+                className="mt-5"
+                key={oIndex}
+                style={{ borderBottom: "5px solid indigo" }}
+              >
+                <h2 className="mb-5">
+                  <span className="bg-primary">Order ID: {o._id}</span>
+                </h2>
+                <ul className="list-group mb-2">
+                  <li className="list-group-item">{o.status}</li>
+                  <li className="list-group-item">
+                    Transaction ID: {o.transaction_id}
+                  </li>
+                  <li className="list-group-item">Amount: ${o.amount}</li>
+                  <li className="list-group-item">Ordered By: {o.user.name}</li>
+                  <li className="list-group-item">
+                    Ordered on: {moment(o.createdAt).fromNow()}
+                  </li>
+                  <li className="list-group-item">
+                    Delivery Address: {o.address}
+                  </li>
+                </ul>
+                <h3 className="mt-4 mb-4 font-italic">
+                  Total products in the order: {o.products.length}
+                </h3>
+
+                {o.products.map((p, index) => (
+                  <div
+                    className="mb-4"
+                    key={index}
+                    style={{ padding: "20px", border: "1px solid indigo" }}
+                  >
+                    {showInput("Product name", p.name)}
+                    {showInput("Product price", p.price)}
+                    {showInput("Product total", p.count)}
+                    {showInput("Product Id", p._id)}
+                  </div>
+                ))}
+              </div>
+            );
+          })}
+          {showOrdersLength()}
         </div>
       </div>
     </Layout>
